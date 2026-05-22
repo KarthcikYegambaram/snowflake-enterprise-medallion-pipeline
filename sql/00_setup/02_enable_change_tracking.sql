5ca0e30731311a9d453500bc453287518ab91c12
Karthick Yegambaram <143865296+KarthcikYegambaram@users.noreply.github.com>
2026-05-22 16:43:50 +0530

    Update 02_enable_change_tracking.sql

--- sql/00_setup/02_enable_change_tracking.sql
+++ sql/00_setup/02_enable_change_tracking.sql
@@ -1,1 +1,7 @@
 
+-- ============================================================
+-- ENABLE CHANGE TRACKING
+-- ============================================================
+
+ALTER TABLE KARTHICKY_DB.PUBLIC.PHARMA_SALES
+SET CHANGE_TRACKING = TRUE;
