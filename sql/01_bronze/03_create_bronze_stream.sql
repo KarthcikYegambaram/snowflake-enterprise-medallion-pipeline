4d6f23bfb4afe065c6ffe1c1aa29adc62b02bcce
Karthick Yegambaram <143865296+KarthcikYegambaram@users.noreply.github.com>
2026-05-22 16:44:51 +0530

    Update 03_create_bronze_stream.sql

--- sql/01_bronze/03_create_bronze_stream.sql
+++ sql/01_bronze/03_create_bronze_stream.sql
@@ -1,1 +1,10 @@
 
+-- ============================================================
+-- BRONZE STREAM
+-- ============================================================
+
+CREATE OR REPLACE STREAM KARTHICKY_DB."1_BRONZE".STREAM_BRONZE_TO_SILVER
+
+ON TABLE KARTHICKY_DB."1_BRONZE".PHARMA_SALES_RAW
+
+SHOW_INITIAL_ROWS = TRUE;
