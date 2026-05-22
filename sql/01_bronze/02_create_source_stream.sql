cab2ea21b57d84fcad7476f4f7b9c6515e66081c
Karthick Yegambaram <143865296+KarthcikYegambaram@users.noreply.github.com>
2026-05-22 16:44:36 +0530

    Update 02_create_source_stream.sql

--- sql/01_bronze/02_create_source_stream.sql
+++ sql/01_bronze/02_create_source_stream.sql
@@ -1,1 +1,10 @@
 
+-- ============================================================
+-- SOURCE STREAM
+-- ============================================================
+
+CREATE OR REPLACE STREAM KARTHICKY_DB.PUBLIC.STREAM_PHARMA_SALES_RAW
+
+ON TABLE KARTHICKY_DB.PUBLIC.PHARMA_SALES
+
+SHOW_INITIAL_ROWS = TRUE;
